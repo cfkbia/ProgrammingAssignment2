bc8bd8123fde70bb9d3e9df7929f0bf7ff6dbbbc
cfkbia <krzysztof.bialkowski@candf.com>
2016-07-11 16:17:39 +0200

    Adding func content

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,21 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  solvedMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    solvedMatrix <<- NULL
+  }
+  
+  get <- function() x
+  
+  setSolveMatrix <- function(solve) solvedMatrix <<- solve
+  
+  getSolveMatrix <- function() solvedMatrix
+  
+  list(set = set, get = get,
+       setSolveMatrix = setSolveMatrix,
+       getSolveMatrix = getSolveMatrix)
 }
 
 
@@ -12,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  solvedMatrix <- x$getSolveMatrix()
+  if(!is.null(solvedMatrix)) {
+    message("getting cached data")
+    return(solvedMatrix)
+  }
+  data <- x$get()
+  solvedMatrix <- solve(data, ...)
+  x$setSolveMatrix(solvedMatrix)
+  solvedMatrix
 }
